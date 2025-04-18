2ae9a54db9390a13a9e9e4b131d4aec0d99e52d5
stebau91 <31169217+stebau91@users.noreply.github.com>
2025-04-18 20:34:08 +0100

    adjust for github static

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter(),
+    paths: {
+      base: '/Svelte-Timer', // 👈 important!
+    },
+    prerender: {
+      default: true
+    }
+  }
+};
