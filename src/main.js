df09c1e6206cb77ffc2abdc9d78f4f3f72097c25
stebau91 <31169217+stebau91@users.noreply.github.com>
2025-04-18 21:07:46 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { mount } from 'svelte'
-import '/Svelte-Timer/src/app.css'
-import App from '/Svelte-Timer/src/App.svelte'
+import './app.css'
+import App from './App.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app'),
